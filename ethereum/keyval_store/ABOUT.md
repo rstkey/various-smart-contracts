d270182e281bd660750dfa902863a4d2b09ec278
Kay <kehiiiiya@gmail.com>
2024-01-23 16:55:01 +0000

    chore(ETH): hello world to key/value store

---
+++ ethereum/keyval_store/ABOUT.md
@@ -0,0 +1,11 @@
+# EVM hello world contract
+
+A simple ethereum contract written in Solidity language. This contract have 3 functions that keep track of a map which is a simple key/value store.
+
+> NOTE: lookup and name function just written as an example of `pure` and `view` functions.
+
+# Contract
+
+The Sepolia testnet deployed example:
+
+https://sepolia.etherscan.io/address/0xafbd78c22d0c523087217d6ccf6305839c8fd81a
---
+++ ethereum/keyval_store/KeyValueStore.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract KeyValueStore {
+    mapping(string => string) public store;
+
+    function set(string memory _Key, string memory _Value) public {
+        store[_Key] = _Value;
+    }
+
+    function name() public pure returns (string memory) {
+        return "key, value store";
+    }
+
+    function lookup(string memory _Key) public view returns (string memory) {
+        return store[_Key];
+    }
+}
