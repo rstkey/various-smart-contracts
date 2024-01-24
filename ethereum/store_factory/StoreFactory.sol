d053c9bc74ab4d7f774fc6f00df643679dff6e64
Kay <kehiiiiya@gmail.com>
2024-01-24 18:28:53 +0000

    fix(EVM): import path in storage factory

--- ethereum/store_factory/StoreFactory.sol
+++ ethereum/store_factory/StoreFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {KeyValueStore} from "keyval_store/KeyValueStore.sol";
+import {KeyValueStore} from "../keyval_store/KeyValueStore.sol";
 
 contract StoreFactory {
     mapping(string => KeyValueStore) public keyValStores;
